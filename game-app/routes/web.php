366f6dfbbc0db8540f924256547d802fd6eeb94f
AdmiralKeso <cas.hagenaar@gmail.com>
2026-09-05 20:25:13 +0200

    Add routes to pages for main menu

--- game-app/routes/web.php
+++ game-app/routes/web.php
@@ -5,3 +5,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('menu');
 });
+
+Route::get('/game/new', function () {
+    return view('createsession');
+});
+
+Route::get('/game/join', function () {
+    return view('joinsession');
+});
+
+Route::get('/game/profile', function () {
+    return view('profile');
+});
+
+Route::get('/game/settings', function () {
+    return view('settings');
+});
+
+Route::get('/game/credits', function () {
+    return view('credits');
+});
